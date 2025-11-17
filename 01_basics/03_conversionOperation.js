fe61c81ec5c50e5bbeed3a09d2b15ddd585d30f4
chandan-git-cs <chandan.csolutions@gmail.com>
2025-11-17 12:35:20 +0000

    comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,21 +1,63 @@
-let score = "33";
-let nan = "33abc";
-let score1 = null;
+// let score = "33";
+// let nan = "33abc";
+// let score1 = null;
 
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let  valInNumber = Number(score);
-console.log(typeof valInNumber);
+// let  valInNumber = Number(score);
+// console.log(typeof valInNumber);
 
-let valInNumber1 = Number(nan);
-console.log(valInNumber1);
+// let valInNumber1 = Number(nan);
+// console.log(valInNumber1);
 
-console.log(score1);
-console.log(typeof (score1));
+// console.log(score1);
+// console.log(typeof (score1));
 
 
 // "33" => 33
 // "33abc" => Nan => Not a number
 // true => 1 ; false => 0
+
+
+// *********************** Operations****************
+
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = "world";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+// prifix and postfix
+
+let num = 20;
+// postfix
+num++;
+console.log(num);
+// prifix
+++num;
+console.log(num);
